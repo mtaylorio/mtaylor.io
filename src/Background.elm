cc104a47b42f27bcb1f11f691f5e8d7047e1bee8
Mike Taylor <mike.charles.taylor@gmail.com>
2025-03-15 14:58:13 -0600

    Dead code cleanup

--- src/Background.elm
+++ src/Background.elm
@@ -92,49 +92,6 @@ bgls palette hide dimensions segments = concat
   ]
 
 
-backgroundLine :
-  Bool -> Bool -> Bool -> Int -> Int -> ColorPalette -> Dimensions -> Svg msg
-backgroundLine top left hide n total palette dimensions =
-  line
-    [ x1 (fromInt (lineCoordinate False (not left) n total dimensions.width))
-    , y1 (fromInt (lineCoordinate True top n total dimensions.height))
-    , x2 (fromInt (lineCoordinate True (not left) n total dimensions.width))
-    , y2 (fromInt (lineCoordinate False (not top) n total dimensions.height))
-    , style (lineStyle hide palette)
-    ]
-    []
-
-
-backgroundLines :
-  Bool -> Bool -> Bool -> Int -> ColorPalette -> Dimensions -> List (String, Svg msg)
-backgroundLines top left hide total palette dimensions =
-  backgroundLines_ top left hide total total palette dimensions
-
-
-backgroundLines_ :
-  Bool -> Bool -> Bool -> Int -> Int -> ColorPalette -> Dimensions -> List (String, Svg msg)
-backgroundLines_ top left hide n total palette dimensions =
-  if n == (total // 2) then
-    []
-  else
-    ( "line-" ++ fromInt n, backgroundLine top left hide n total palette dimensions )
-    :: backgroundLines_ top left hide (n - 1) total palette dimensions
-
-
-lineCoordinate : Bool -> Bool -> Int -> Int -> Int -> Int
-lineCoordinate iter rev n total dimension =
-  if iter then
-    if rev then
-      dimension - (n * dimension // total)
-    else
-      n * dimension // total
-  else
-    if rev then
-      dimension
-    else
-      0
-
-
 lineStyle : Bool -> ColorPalette -> String
 lineStyle hide palette = "stroke: " ++ palette.secondaryForegroundColor
   ++ "; transition: all 10s linear; stroke-width: 1px; stroke-dasharray: 5000"
